8bb23bc3f673179146dede6c778d580dac2b3768
gorankoc <goooran.k@gmail.com>
2020-12-03 23:20:12 +0100

    Cleanup code, new branch DI - initial ... starting

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -7,29 +7,26 @@ namespace Commander.Controllers
 {
 	[Route("api/commands")]
 	[ApiController]
-	public class CommandsController:ControllerBase
-	{
-		//3. not good way just for mock ... and fast start 
-		private readonly MockCommandRepo _repository = new MockCommandRepo();
+    public class CommandsController:ControllerBase
+    {
+		private readonly MockCommandRepo _repository = new MockCommandRepo();//3. not good way just for mock ... and fast start 
 
 		[HttpGet]
 		public ActionResult <IEnumerable<Command>> GetAllCommands(){
 			var commandItems = _repository.GetAllCommands();
 			return Ok(commandItems);
 		}
-		// 4. GET api/commands/{id}
+		
 		[HttpGet("{id}")]
-		public ActionResult <Command> GetCommandById(int id){
+		public ActionResult <Command> GetCommandById(int id){// 4. GET api/commands/{id}
 			var commandItem = _repository.GetCommandById(id);
 			return Ok(commandItem);			
 		}
-	}
+    }
 }
 
 /*
-
 1. apicontroller atrtibut - out of the box behaviours
-
 2. Route - hoe to get ot resources - basic Controller level
 
 other example:
@@ -45,19 +42,15 @@ call it should hit this endpoint.
 3. use repository and pull that data and repesent to the user
 
 4. GET api/commands/{id}
-
    when we request via this endopoint it will be
    resolved via URI ... That comes from somthing called
    Binding Sourdces ( Model Binding for HTTP GET requests )
    from query, ROUTE, form, body, Header.
 
-   WHAT EVER ID u put api/commands/100 it allways return id 0
-   from MockCommandRepo
-			{
+   WHAT EVER ID u put api/commands/100 it allways return 
    				Id = 0,
 				HowTo = "Boil an egg",
 				Line = "Boil Water",
 				Platform = "Pan"
-			}
 
 */
