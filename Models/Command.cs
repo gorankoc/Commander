cf452a5eeac458256e6c2bd1f6bb1355ca0ae9a4
gorankoc <goooran.k@gmail.com>
2020-12-04 05:52:49 +0100

    EF DbContext SqlExpress Creating a db success

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Models
 {
-	public class Command
-	{
-		public int Id { get; set; }
-		public string HowTo { get; set; }
-		public string Line { get; set; }
-		public string Platform { get; set; }
-	}
+    public class Command
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+        [Required]
+        public string Line { get; set; }
+        [Required]
+        public string Platform { get; set; }
+    }
 }
